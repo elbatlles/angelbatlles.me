ff6658f877a75b2b0a6c50ea086ffd019d7ba97d
angel <angel.batlles@kumux.io>
2021-04-03 22:39:43 +0200

    seo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,23 @@
 import React from "react";
 import Navbar from "../Navbar";
+import { useLocation } from "react-router-dom";
 import SEO from "../seo";
+import { menuText } from "../../utils";
 
 export const Header = (props) => {
+  const location = useLocation();
+  console.log(location.pathname);
+  const seoConfig = menuText.find((config) => {
+    return config.url === location.pathname.replace("/", "");
+  });
+
   return (
     <>
       <SEO
-        description="[Angel Batlles] 🥷🏻 | Mi recorrido"
+        description={seoConfig ? seoConfig.description : "Pagina no encontrada"}
         lang="es"
-        meta="[Angel Batlles] 🥷🏻 | Mi recorrido"
-        title="[Angel Batlles] 🥷🏻 | Mi recorrido"
+        meta={seoConfig ? seoConfig.meta : "Pagina no encontrada"}
+        title={seoConfig ? seoConfig.title : "Pagina no encontrada"}
       />
       <Navbar transparent />
     </>
